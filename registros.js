45a35b839d701d374067c4a45321745de8e83b28
rafaellindemann <rafaelld@gmail.com>
2023-09-22 10:02:37 -0300

    Iniciando a criação da página principal das vacinas

---
+++ registros.js
@@ -0,0 +1,10 @@
+// {nome: '', categoria: '', descricao: '', link: '', tags: []},
+const vacinas = [
+    {nome: 'Curso em Vídeo, HTML5 e CSS3, Módulo 1/5', categoria: 'Curso Youtube', descricao: 'O primeiro módulo do melhor curso de front para iniciantes, pelo melhor professor do mundo.', link: 'https://www.youtube.com/watch?v=Ejkb_YpuHWs&list=PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n&ab_channel=CursoemV%C3%ADdeo', tags: ['HTML', 'CSS', 'Curso', 'Youtube', 'Guanabara', 'Curso em vídeo', 'Senta a assiste']},
+    {nome: 'Curso em Vídeo, HTML5 e CSS3, Módulo 2/5', categoria: 'Curso Youtube', descricao: 'O primeiro módulo do melhor curso de front para iniciantes, pelo melhor professor do mundo.', link: 'https://www.youtube.com/watch?v=vPNIAJ9B4hg&list=PLHz_AreHm4dlUpEXkY1AyVLQGcpSgVF8s&ab_channel=CursoemV%C3%ADdeo', tags: ['HTML', 'CSS', 'Curso', 'Youtube', 'Guanabara', 'Curso em vídeo', 'Senta a assiste']},
+    {nome: 'Curso em Vídeo, HTML5 e CSS3, Módulo 3/5', categoria: 'Curso Youtube', descricao: 'O primeiro módulo do melhor curso de front para iniciantes, pelo melhor professor do mundo.', link: 'https://www.youtube.com/watch?v=ofFgnDtn_1c&list=PLHz_AreHm4dmcAviDwiGgHbeEJToxbOpZ&ab_channel=CursoemV%C3%ADdeo', tags: ['HTML', 'CSS', 'Curso', 'Youtube', 'Guanabara', 'Curso em vídeo', 'Senta a assiste']},
+    {nome: 'Curso em Vídeo, HTML5 e CSS3, Módulo 4/5', categoria: 'Curso Youtube', descricao: 'O primeiro módulo do melhor curso de front para iniciantes, pelo melhor professor do mundo.', link: 'https://www.youtube.com/watch?v=zHKHMmEG9vE&list=PLHz_AreHm4dkcVCk2Bn_fdVQ81Fkrh6WT', tags: ['HTML', 'CSS', 'Curso', 'Youtube', 'Guanabara', 'Curso em vídeo', 'Senta a assiste']},
+]
+
+
+
