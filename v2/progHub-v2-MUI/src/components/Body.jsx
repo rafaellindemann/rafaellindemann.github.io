d945a1d491955324726690982d5bedae71866e14
rafaellindemann <rafaelld@gmail.com>
2025-02-28 21:47:40 -0300

    fix layout

--- v2/progHub-v2-MUI/src/components/Body.jsx
+++ v2/progHub-v2-MUI/src/components/Body.jsx
@@ -36,25 +36,3 @@ function Body() {
 export default Body;
 
 
-// import './Body.css'
-// import { useContext } from "react";
-// import { GlobalContext } from '../contexts/GlobalContext'
-// import Card from './Card';
-
-// function Body() {
-//   const { categories, resources, handleFilter,filteredResources, setFilteredResources } = useContext(GlobalContext)
-//   return (
-//     <div className="container-body">
-
-//       <div className="registros">
-//         {filteredResources.map((resource) => (
-//           <Card r={resource} key={resource.id}/>
-//         ))}
-
-//       </div>
-      
-//     </div>
-//   )
-// }
-
-// export default Body
